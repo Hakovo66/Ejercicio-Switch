58f43f68ca2d9b95ffe44f768348da64d6f316c4
Hakovo66 <113375465+Hakovo66@users.noreply.github.com>
2023-04-30 23:13:19 -0600

    Create Ejercicio-Switch.java

---
+++ Ejercicio-Switch.java
@@ -0,0 +1,33 @@
+public class EjercicioSwitch {
+
+   
+    public static void main(String[] args){
+    
+    var estacion = "verano";
+
+    switch (estacion) {
+        case "verano":
+        System.out.println("es verano");
+        break;
+
+        case "invierno":
+        System.out.println("es invierno");
+        break;
+
+        case "primavera":
+        System.out.println("es primavera");
+        break;
+
+        case "otono":
+        System.out.println("es otoño");
+        break;
+
+        default :
+        System.out.println("estoy en default");
+
+    }
+
+
+     }
+    
+}
